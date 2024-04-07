22dd3b75350588c6c2a03ccd3c8851eba60a9ad1
Stijn van Dongen <svd@expressionedits.com>
2024-04-07 16:43:13 +0000

    add parameters to randomly change sequences

--- utils/makedna.pl
+++ utils/makedna.pl
@@ -6,8 +6,19 @@ use strict;
 my %map = (' ', qw(G e C t T a A o CT i CA n AA s C r TA h AT d AG l GG u GA c TG m GC f TT y AC w GT g CT p CA b AA v CC k TC x TA q AT j AG z CG));
 $map{'<'} = '<';
 $map{'>'} = '>';
+$map{'{'} = '{';
+$map{'}'} = '}';
 
 my %predef = ();
+my ($rc, $ri, $rd, $indelmaxsize) = (0, 0, 0, 0);  #  rate of change, insert, deletion
+
+if (@ARGV == 4) {
+  ($rc, $ri, $rd, $indelmaxsize) = @ARGV;
+  @ARGV = ();
+}
+elsif (@ARGV) {
+  die "Use as pipe, optional give <base change rate> <insertion rate> <deletion rate> <indelmaxsize> as command line arguments";
+}
 
 while (<DATA>) {
    chomp;
@@ -21,6 +32,37 @@ sub rc {
    my $dna = shift;
    $dna =~ tr /ACGT/TGCA/;
    return reverse($dna);
+}
+
+sub rb {
+   return qw( A C G T )[int(rand(4))];
+}
+
+sub muck {                               # just affect some change. no model here.
+  my $seq = shift;
+  my $new = "";
+  for (my $i=0; $i<length($seq); $i++) {
+    if (rand(1) < $rc) {                 # change a base
+      $new .= rb();
+    }
+    my $do_ins = rand(1) < $ri;
+    my $do_del = rand(1) < $rd;
+    my $ni = 1 + int($indelmaxsize * rand(1)*rand(1));
+    my $nd = 1 + int($indelmaxsize * rand(1)*rand(1));
+    if (!$do_ins && !$do_del) {
+      $new .= substr($seq, $i, 1);
+    }
+    else {
+      if ($do_ins) {
+        $new .= substr($seq, $i, 1);
+        $new .= rb() for 1..$ni;
+      }
+      if ($do_del) {
+        $i += $nd;
+      }
+    }
+  }
+  return $new;
 }
 
 while (<>) {
@@ -41,6 +83,7 @@ while (<>) {
       $seq .= $bp if defined($bp);
    }
    $seq =~ s/\<([ACGT]+)\>/rc($1)/ge;
+   $seq =~ s/\{([ACGT]+)\}/muck($1)/ge;
    print ">$name\n$seq\n";
 }
 
