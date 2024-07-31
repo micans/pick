b2deb53e0ea7aca61a3bc646eda61a8ca1125ba2
Stijn van Dongen <micans@users.noreply.github.com>
2024-07-31 13:54:18 +0100

    with --sam-trail=N print context of N bases around alignments with ,aln_all etc

--- utils/samdisplaymatches.sh
+++ utils/samdisplaymatches.sh
@@ -8,7 +8,7 @@ FASTA=${1?Need fasta}
 
 # pick --sam/"$FASTA" ::^edit=,alnedit^%20flags=:2^%20patchi=,aln_aln^100,patchiness^%20maxi=:6^I,cgmax,catall:3:1,aln_ref,aln_aln,aln_qry^^%09,joinall | pick -k ::^:'.*'^%0A,joinall
 
-pick --sam/"$FASTA" nedit:=,alnedit npatchi:=,aln_aln^100,patchiness insmax:=6^I,cgmax \
+pick --sam/"$FASTA" --sam-trail=0 nedit:=,alnedit npatchi:=,aln_aln^100,patchiness insmax:=6^I,cgmax \
       qc:=^nedit=:nedit^%20npatchi=:npatchi^%20insmax=:insmax,catall \
-      ::qc:3:1,aln_ref,aln_aln,aln_qry^^^%0A,joinall
+      ::qc:3:1,aln_all^^^%0A,joinall
 
