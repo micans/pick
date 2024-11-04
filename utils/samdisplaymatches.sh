b691dd92dff7a6fd42fc29c6380d09461073959a
Stijn van Dongen <micans@users.noreply.github.com>
2024-11-04 16:16:04 +0000

    clean-ups, reinstate aln_all_pack

--- utils/samdisplaymatches.sh
+++ utils/samdisplaymatches.sh
@@ -4,11 +4,31 @@
 
 set -euo pipefail
 
-FASTA=${1?Need fasta}
+FASTA=${1?I need fasta as first argument and SAM format on STDIN}
 
-# pick --sam/"$FASTA" ::^edit=,alnedit^%20flags=:2^%20patchi=,aln_aln^100,patchiness^%20maxi=:6^I,cgmax,catall:3:1,aln_ref,aln_aln,aln_qry^^%09,joinall | pick -k ::^:'.*'^%0A,joinall
-
-pick --sam/"$FASTA" --sam-trail=0 nedit:=,alnedit npatchi:=,aln_aln^100,patchiness insmax:=6^I,cgmax \
+pick --sam/"$FASTA" --sam-aln-context=0   \
+      nedit:=,aln_nedit                   \
+      npatchi:=,aln_aln^40,patchiness     \
+      insmax:=6^I,cgmax                   \
       qc:=^nedit=:nedit^%20npatchi=:npatchi^%20insmax=:insmax,catall \
-      ::qc:3:1,aln_all^^^%0A,joinall
+                                          \
+      nedit ::qc:3:1,aln_all_pack^%01^%09,joinall | sort -nr | pick -k 2- | tr '\1\t' '\n'
+
+
+# The approach below avoids packing with aln_all_pack and unpacking with tr as above.
+# However it is lossy, as sorting means we may loose
+# query sequences if the primary sam hit sorts below non-primary hits.
+
+#    pick --sam -A0 nedit::,aln_nedit     \
+#  | sort -nr                    \
+#  | cut -f 2-                   \
+#  | pick --sam/$FASTA        \
+#        nedit:=,aln_nedit                   \
+#        npatchi:=,aln_aln^100,patchiness    \
+#        insmax:=6^I,cgmax                   \
+#        qc:=^nedit=:nedit^%20npatchi=:npatchi^%20insmax=:insmax,catall \
+#                                            \
+#        ::qc:3:1,aln_all^^^%0A,joinall
+
+
 
